61b460b8b947bd63c1ca0148fa93653393538aa4
Bibounet <bibou.ytb33@gmail.com>
2026-04-27 16:50:14 +0200

    merge!

--- Start.java
+++ Start.java
@@ -7,13 +7,21 @@ public class Start {
 
     public static void main(String[] args) {
 
-
+        //test capsules
         CapsulesMaker c = new Orion();
         System.out.println(c.getDescription());
         System.out.println(c.canCarryHumans());
         System.out.println(c.getMaxPeople());
         System.out.println(c.getWeight());
         System.out.println(c.getPrice());
+        //test launcher
+        Launchers l  = new Falcon9();
+        System.out.println(l.getName());
+        System.out.println(l.canCarryHumans());
+        System.out.println(l.getMaxFuel());
+        System.out.println(l.getDescription());
+
+
 
 
 
